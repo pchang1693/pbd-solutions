32abccc7799effd9f051d8e96eec4d17961be02e
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-02-15 12:32:44 -0500

    Fixed style

--- 06/LetterToYourself.java
+++ 06/LetterToYourself.java
@@ -1,5 +1,5 @@
-public class LetterToYourself{
-    public static void main (String[] args){
+public class LetterToYourself {
+    public static void main(String[] args) {
 
         System.out.println("--------------------------------------------------------");
         System.out.println("|                                                ##### |");
