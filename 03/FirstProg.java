45a2b112e202bf65abe1982e1dc59b523a411242
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-02-15 12:35:31 -0500

    Fixed style

--- 03/FirstProg.java
+++ 03/FirstProg.java
@@ -1,5 +1,5 @@
-public class FirstProg{
-    public static void main(String[] args){
+public class FirstProg {
+    public static void main(String[] args) {
         System.out.println("Mr. Gallo is cool.");
     }
 }
