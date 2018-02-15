c8538056332246923c6e91cbdce7d094eeb86870
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-02-15 13:13:37 -0500

    Uploaded solution to Using Variables

---
+++ 11/UsingVariables.java
@@ -0,0 +1,13 @@
+public class UsingVariables {
+    public static void main(String[] args) {
+        
+        int room = 113;
+        double e = 2.71828;
+        String subject = "Computer Science";
+
+        System.out.println("This is room # " + room);
+        System.out.println("e is close to " + e);
+        System.out.println("I am leaning a bit about " + subject);
+        
+    }
+}
