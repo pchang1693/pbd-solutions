25f81c32d2404a4de2018c53e4a91bdd71194b70
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-02-16 13:46:40 -0500

    Uploaded solution to Still Using Variables

---
+++ 12/StillUsingVariables.java
@@ -0,0 +1,10 @@
+public class StillUsingVariables {
+    public static void main(String[] args) {
+        
+        String name = "Portia Chang";
+        int grad = 2018;
+        
+        System.out.println("My name is " + name + " and I'll graduate in " + grad + ".");
+        
+    }
+}
