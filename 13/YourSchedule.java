69ec088063e260c57506ba3a5f78ea7e8ac02134
Portia Chang <32988308+pchang1693@users.noreply.github.com>
2018-02-20 12:45:53 -0500

    Uploaded solutions to Your Schedule

---
+++ 13/YourSchedule.java
@@ -0,0 +1,20 @@
+public class YourSchedule {
+    public static void main(String[] args) {
+        String classOne = "Chemistry";
+        String classTwo = "Biology";
+        String classThree = "Computer Science";
+        String classFour = "Calculus";
+        
+        String teacherOne = "Ms. Alma";
+        String teacherTwo = "Ms. Brock";
+        String teacherThree = "Mr. Carris";
+        String teacherFour = "Ms. De Bronte";
+        
+        System.out.println("---------------------------------------");
+        System.out.println("1   " + classOne + "           " + teacherOne);
+        System.out.println("2   " + classTwo + "             " + teacherTwo);
+        System.out.println("3   " + classThree + "    " + teacherThree);
+        System.out.println("4   " + classFour + "            " + teacherFour);
+        
+    }
+}
